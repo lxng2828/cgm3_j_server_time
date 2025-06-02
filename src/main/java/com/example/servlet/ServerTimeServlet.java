1269a29dfdcee47b822c664eb4f29ce6ba116cf6
vtlxng2828 <vtlxng2828@example.org>
2025-06-02 10:24:22 +0700

    first commit

---
+++ src/main/java/com/example/servlet/ServerTimeServlet.java
@@ -0,0 +1,25 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", urlPatterns = "/serve_time")
+public class ServerTimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("");
+        writer.close();
+    }
+}
---
+++ target/server_time/index.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+  <!DOCTYPE html>
+  <html lang="vi">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chính - Dự án Server Time</title>
+  </head>
+
+  <body>
+    <h2>Chào mừng đến với dự án Server Time!</h2>
+    <p><a href="${pageContext.request.contextPath}/serve_time">Xem thời gian server</a></p>
+  </body>
+
+  </html>
